2033e02447ff25983e4a29b5a2a260b0c93dea33
CORPORATE\mahallem <mahesh.mahalle@act.org>
2019-12-29 09:08:41 -0600

    Add project files.

---
+++ Data/MoviesDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesWebApi.Models;
+
+namespace MoviesWebApi.Data
+{
+    public class MoviesDBContext :DbContext
+    {
+        public MoviesDBContext(DbContextOptions<MoviesDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
